d40df1bef20f97d2e611d72b8ad2f4145576deaf
Azin Katiraee <azinnash@yahoo.com>
2022-10-06 15:47:10 -0400

    adding production=false into index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+production=false;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'main'});
